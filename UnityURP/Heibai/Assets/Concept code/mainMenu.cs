73cdd997e332ca8a4b54ea5361cbed18529ad2e6
PhobosPerdition <adam.omarsh2003@gmail.com>
2023-04-07 17:21:18 +0800

    Menu Mostly Finished
    Ready for being fully integrated with code

--- UnityURP/Heibai/Assets/Concept code/mainMenu.cs
+++ UnityURP/Heibai/Assets/Concept code/mainMenu.cs
@@ -10,26 +10,39 @@ public class mainMenu : MonoBehaviour
     public AudioMixer SFXMixer;
     public AudioMixer musicMixer;
     
+    //basic code to load from menu scene to game scene. In future this will load you into the level select screen directly.
     public void startGame ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    //starter code for muting the SFX from menu screen
     public void muteGame(float SFX)
     {
         Debug.Log("SOUND");
         SFXMixer.SetFloat("SFXVolume", SFX);
     }
 
+    //starter code for muting the music from menu screen
     public void muteMusic (float music)
     {
         Debug.Log("MUSIC");
         musicMixer.SetFloat("musicVolume", music);
     }
 
+    //quits the game when the quit button is pressed
     public void quitGame ()
     {
         Debug.Log("QUIT!");
         Application.Quit();
     }//
+
+    void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.Escape))
+        {
+            //deactivate menu object and activate quit menu object
+            Debug.Log("quit menu");
+        }
+    }
 }
