8e756b95b48c148262fa17e1d0fda4ad162b3cd2
PhobosPerdition <adam.omarsh2003@gmail.com>
2023-04-07 15:02:30 +0800

    Sound Buttons Basics Done
    Implemented the audio mixers for the sound buttons. They do something, not sure what, but they are implemented and ready to work for muting and unmuting. I'm going to also commit the sprites next.

--- UnityURP/Heibai/Assets/Concept code/mainMenu.cs
+++ UnityURP/Heibai/Assets/Concept code/mainMenu.cs
@@ -2,22 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class mainMenu : MonoBehaviour
 {
+    public AudioMixer SFXMixer;
+    public AudioMixer musicMixer;
+    
     public void startGame ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    public void muteGame ()
+    public void muteGame(float SFX)
     {
         Debug.Log("SOUND");
+        SFXMixer.SetFloat("SFXVolume", SFX);
     }
 
-    public void muteMusic ()
+    public void muteMusic (float music)
     {
         Debug.Log("MUSIC");
+        musicMixer.SetFloat("musicVolume", music);
     }
 
     public void quitGame ()
