9949f78ee754b5b7da4f884e80ff27387d43dadb
MorganMcLaren <morgs457@gmail.com>
2023-11-15 18:03:15 +0800

    prep for save states

---
+++ UnityURP/Heibai_URP/Assets/Heibei/Scripts/SaveStars.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SaveStars : MonoBehaviour
+{
+
+    int levelStars;
+
+    
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+    /*public int level1Save()
+    {
+        levelStars = StarPickups.starsCollected;
+
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("Level_1"))
+        {
+            dafb.
+        }
+
+
+
+
+
+
+    }
+
+
+    public int level1Load()
+    {
+
+    }*/
+}
